bb93f2fb0b71e8c378b7dcf9f642ad663225d594
Johnlee <fgfg56784#126.com>
2019-05-06 19:12:00 +0800

    study python and html 2019/05/06 19:10

---
+++ nsd1811/devweb/day4/mysite1/polls2/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'),
+# ]
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/result/$', views.result, name='result'),
+]
